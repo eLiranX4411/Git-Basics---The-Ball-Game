8e13caf2c7aea0ecae7060739dd02d5cc3732391
eLiranX4411 <solidityart@gmail.com>
2024-06-18 13:32:22 +0300

    Revert "Fix ball sizing function
    This reverts commit 2049bb0269f56cd2f63a099ffe7e3184dc676870.

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 'use strict'
 
-var gBall1Size = 100
-
 function onBallClick() {
-  var elBall1 = document.querySelector('.ball')
-
-  gBall1Size += 50
-  if (gBall1Size > 400) gBall1Size = 100
-
-  elBall1.style.width = gBall1Size + 'px'
-  elBall1.style.height = gBall1Size + 'px'
-  elBall1.innerText = gBall1Size
+  var elBall = document.querySelector('.ball')
+  elBall.style.height += '150px'
+  elBall.style.width += '150px'
+  elBall.innerText = '150'
 }

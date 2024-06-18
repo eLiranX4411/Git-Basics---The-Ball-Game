3e617c1ea20d832db7137d439c51d5049485f3f2
eLiranX4411 <solidityart@gmail.com>
2024-06-18 13:35:39 +0300

    Fix ball sizing & Limit the ball size diameter

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 'use strict'
 
+var gBall1Size = 100
+
 function onBallClick() {
-  var elBall = document.querySelector('.ball')
-  elBall.style.height += '150px'
-  elBall.style.width += '150px'
-  elBall.innerText = '150'
+  var elBall1 = document.querySelector('.ball')
+
+  gBall1Size += 50
+  if (gBall1Size > 400) gBall1Size = 100
+
+  elBall1.style.width = gBall1Size + 'px'
+  elBall1.style.height = gBall1Size + 'px'
+  elBall1.innerText = gBall1Size
 }

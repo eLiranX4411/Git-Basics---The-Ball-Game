dd16c508f6009fe3cf24fb22cf4097c7d588426f
eLiranX4411 <solidityart@gmail.com>
2024-06-18 13:56:27 +0300

    Add util script & getRandomInt diameter

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var gBall1Size = 100
 function onBallClick() {
   var elBall1 = document.querySelector('.ball')
 
-  gBall1Size += 50
+  gBall1Size += getRandomInt(20, 60)
   if (gBall1Size > 400) gBall1Size = 100
 
   elBall1.style.width = gBall1Size + 'px'

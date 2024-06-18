59f48e2f6d93d025e4512c17b1471fab92ccf3c8
eLiranX4411 <solidityart@gmail.com>
2024-06-18 15:05:09 +0300

    Fix css styling to balls & Add script to Ball_2

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 var gBall1Size = 100
 var gBall2Size = 100
 
-function onBallClick() {
+function onBallClick(maxDiameter) {
   var elBall1 = document.querySelector('.ball')
 
   gBall1Size += getRandomInt(20, 60)
-  if (gBall1Size > 400) gBall1Size = 100
+  if (gBall1Size > maxDiameter) gBall1Size = 100
 
   elBall1.style.width = gBall1Size + 'px'
   elBall1.style.height = gBall1Size + 'px'
@@ -15,4 +15,14 @@ function onBallClick() {
   elBall1.style.backgroundColor = getRandomColor()
 }
 
-function onBall2Click() {}
+function onBall2Click(maxDiameter) {
+  var elBall2 = document.querySelector('.ball_2')
+
+  gBall2Size += getRandomInt(20, 60)
+  if (gBall2Size > maxDiameter) gBall2Size = 100
+
+  elBall2.style.width = gBall2Size + 'px'
+  elBall2.style.height = gBall2Size + 'px'
+  elBall2.innerText = gBall2Size
+  elBall2.style.backgroundColor = getRandomColor()
+}
